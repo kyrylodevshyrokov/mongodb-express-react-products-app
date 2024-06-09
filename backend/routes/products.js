8d2eca206d6798bb3906c3620b3c2b1abca375f9
Kreal11 <kyrylo.shyrokov.dev@gmail.com>
2024-06-10 00:21:14 +0300

    created Index on price for faster sorting

--- backend/routes/products.js
+++ backend/routes/products.js
@@ -10,10 +10,8 @@ const router = Router();
 
 // Get list of products products
 router.get("/", (req, res, next) => {
-  // Return a list of dummy products
-  // Later, this data will be fetched from MongoDB
-  // const queryPage = req.query.page;
-  // const pageSize = 5;
+  const queryPage = req.query.page;
+  const pageSize = 1;
   // let resultProducts = [...products];
   // if (queryPage) {
   //   resultProducts = products.slice(
@@ -27,6 +25,9 @@ router.get("/", (req, res, next) => {
     .db()
     .collection("products")
     .find()
+    .sort({ price: -1 })
+    // .skip((queryPage - 1) * pageSize)
+    // .limit(pageSize)
     .toArray()
     .then((productDocs) => {
       productDocs.forEach((doc) => {
